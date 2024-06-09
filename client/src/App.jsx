f0fdaabb045cf5e830f3e12e455a5664d2701acb
Surakit <surakitkunkayun@gmail.com>
2024-06-10 05:39:26 +0700

    feat: finished assignment

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,29 +1,74 @@
 import "./App.css";
+import axios from "axios";
+import { useState, useEffect } from "react";
 
 function App() {
+  const [productData, setProductData] = useState([]);
+  const [loadingStatus, setLoadingStatus] = useState(null);
+
+  useEffect(() => {
+    getProducts();
+  }, []);
+
+  const getProducts = async () => {
+    try {
+      setLoadingStatus("loading");
+      const result = await axios.get("http://localhost:4001/products");
+      setLoadingStatus("completed");
+      setProductData(result.data.data);
+    } catch {
+      setLoadingStatus("error");
+    }
+  };
+
+  const handleDelete = async (id) => {
+    try {
+      await axios.delete(`http://localhost:4001/products/${id}`);
+      const response = productData.filter((item) => {
+        return item.id !== id;
+      });
+      setProductData(response);
+    } catch {
+      setLoadingStatus("failed");
+    }
+  };
+
   return (
     <div className="App">
       <div className="app-wrapper">
         <h1 className="app-title">Products</h1>
       </div>
       <div className="product-list">
-        <div className="product">
-          <div className="product-preview">
-            <img
-              src="https://via.placeholder.com/350/350"
-              alt="some product"
-              width="350"
-              height="350"
-            />
-          </div>
-          <div className="product-detail">
-            <h1>Product name: ...</h1>
-            <h2>Product price: ... Baht</h2>
-            <p>Product description: .....</p>
-          </div>
+        {loadingStatus === "loading" && <h1>Loading...</h1>}{" "}
+        {loadingStatus === "failed" && <h1>Fail to load data...</h1>}
+        {loadingStatus === "completed" &&
+          productData.map((product) => (
+            <div className="product" key={product.id}>
+              <div className="product-preview">
+                <img
+                  src={product.image}
+                  alt="some product"
+                  width="350"
+                  height="350"
+                />
+              </div>
+              <div className="product-detail">
+                <h1>Product name: {product.name}</h1>
+                <h2>Product price: {product.price} Baht</h2>
+                <p>Product description: {product.description}</p>
+              </div>
 
-          <button className="delete-button">x</button>
-        </div>
+              <button
+                className="delete-button"
+                onClick={() => handleDelete(product.id)}
+              >
+                x
+              </button>
+            </div>
+          ))}
+          {loadingStatus === "completed" && !productData.length && (
+        <h1>No Product!</h1>
+      )}
       </div>
     </div>
   );
